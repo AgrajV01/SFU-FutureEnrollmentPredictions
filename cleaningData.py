7f87f62d8240b4e7c813e7fd4bc7782b444a6a33
Agraj Vuppula <ava42@sfu.ca>
2024-07-26 16:44:55 -0700

    Major update on cleaning data

--- cleaningData.py
+++ cleaningData.py
@@ -1,68 +1,54 @@
 # In this file we are going filter the data we need and write them into a new file. 
+# taken some reference from https://stackoverflow.com/questions/58295555/pandas-append-new-row-with-a-different-number-of-columns
 import numpy as np
 import pandas as pd
 
-import sys
-
-# table_schema = types.StructType([
-#     types.StructField('Course', types.StringType()),
-#     types.StructField('Course Number', types.StringType()),
-#     types.StructField('2019-02', types.IntegerType()),
-#     types.StructField('2019-03', types.IntegerType()),
-#     types.StructField('2020-01', types.IntegerType()),
-#     types.StructField('2020-02', types.IntegerType()),
-#     types.StructField('2020-03', types.IntegerType()),
-#     types.StructField('2021-01', types.IntegerType()),
-#     types.StructField('2021-02', types.IntegerType()),
-#     types.StructField('2021-03', types.IntegerType()),
-#     types.StructField('2022-01', types.IntegerType()),
-#     types.StructField('2022-02', types.IntegerType()),
-#     types.StructField('2022-03', types.IntegerType()),
-#     types.StructField('2023-01', types.IntegerType()),
-#     types.StructField('2023-02', types.IntegerType()),
-#     types.StructField('2023-03', types.IntegerType()),
-#     types.StructField('2024-01', types.IntegerType()),
-#     types.StructField('2024-02', types.IntegerType())
-
-# ])
-
-
-# def main(in_directory, out_directory):
+def cleanData(data):
+    data['Date'] = pd.to_datetime(data['Date'])
+        
+    # Kepping only the last date Enrolment Status as we dont need the prior date enrolment reports
+    max_date = data['Date'].max()
+    data = data[data['Date'] == max_date]
+    
+    # Keeping only the columns we need
+    data = data[['Subject', 'CatNbr', 'Course Title', 'Sect', 'Type', 'ActEnrol', 'Location']]
+    return data
+    
 def main():
     
     dataFilesList = [
-    'database.1194.xlsx', 'database.1197.xlsx', 'database.1201.xlsx', 'database.1204.xlsx', 
-    'database.1207.xlsx', 'database.1211.xlsx', 'database.1214.xlsx', 'database.1217.xlsx', 
-    'database.1221.xlsx', 'database.1224.xlsx', 'database.1227.xlsx', 'database.1231.xlsx', 
-    'database.1234.xlsx', 'database.1237.xlsx', 'database.1241.xlsx', 'database.1244.xlsx', 
-    'database.1247.xlsx'
-    ]
+    'database.1194.xlsx','database.1197.xlsx', 'database.1201.xlsx', 'database.1204.xlsx', 'database.1207.xlsx', 
+    'database.1211.xlsx', 'database.1214.xlsx', 'database.1217.xlsx', 'database.1221.xlsx', 
+    'database.1224.xlsx', 'database.1227.xlsx', 'database.1231.xlsx', 'database.1234.xlsx', 
+    'database.1237.xlsx', 'database.1241.xlsx', 'database.1244.xlsx', 'database.1247.xlsx']
     
-    finalDataFile = []
-    
+    semName = ['Summer 2019','Fall 2019','Spring 2020','Summer 2020', 'Fall 2020', 'Spring 2021','Summer 2021', 'Fall 2021', 
+        'Spring 2022','Summer 2022', 'Fall 2022','Spring 2023','Summer 2023', 'Fall 2023', 'Spring 2024','Summer 2024','Fall 2024']
+    i =0
+    dataFile =[]
     for file in dataFilesList:
+        
         # Removing the first 6 rows, since that data isnt required.
         data = pd.read_excel(f'data/{file}', header=6)
-        
         data = data.reset_index(drop=True)
-        data['Date'] = pd.to_datetime(data['Date'])
-        
-        # Kepping only the last date Enrolment Status as we dont need the prior date enrolment reports
-        max_date = data['Date'].max()
-        data = data[data['Date'] == max_date]
-        
+
         # only the last file, has the column header name with Section instead of Sect, so modifying according to that
         if file == 'database.1247.xlsx':
             data.rename(columns={'Section': 'Sect'}, inplace=True)
         
-        # Keeping only the columns we need
-        data = data[['Subject', 'CatNbr', 'Course Title', 'Sect', 'Type', 'ActEnrol', 'Location']]
+        data = cleanData(data)
         
-        finalDataFile.append(data)
-        # print(data)
+        # Rename the ActEnrol column to the semester name
+        data.rename(columns={'ActEnrol': semName[i]}, inplace=True)
+        dataFile.append(data)
+        i =i+1
 
+    # print(dataFile)
+    
+    # finally create a new excel file with the cleaned data in it.
+    finalDataFile = pd.concat(dataFile, ignore_index=True)
+    finalDataFile = finalDataFile.groupby(['Subject', 'CatNbr', 'Course Title', 'Sect', 'Type', 'Location'], as_index=False).first()
+    finalDataFile.to_excel('FinalDataFile.xlsx', index=False)
+    
 if __name__=='__main__':
-    # in_directory = sys.argv[1]
-    # out_directory = sys.argv[2]
-    # main(in_directory, out_directory)
     main()
